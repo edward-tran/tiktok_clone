bb034df4fae447f11f31b1a379d42877f9f9bf57
edward-tran <sangtranwp@gmail.com>
2024-12-19 15:46:39 +0700

    Continue make video side

--- src/components/Icon/index.js
+++ src/components/Icon/index.js
@@ -285,23 +285,92 @@ export const LikeIcon = ({ width = '25px', height = '25px' }) => (
         </defs>
     </svg>
 );
-// export const CommentIcon = ({ width = '25px', height = '25px' }) => (
-//     <svg
-//         xmlns="http://www.w3.org/2000/svg"
-//         width="24"
-//         height="24"
-//         fill="currentColor"
-//     >
-//         <use href="#Bubble_Ellipsis_Right_Fill-7ce6d3b3"></use>
-//     </svg>
-// );
-// export const SaveIcon = ({ width = '25px', height = '25px' }) => (
-//     <svg
-//         xmlns="http://www.w3.org/2000/svg"
-//         width="24"
-//         height="24"
-//         fill="currentColor"
-//     >
-//         <use xlink:href="#uncollect-7652bb5c"></use>
-//     </svg>
-// );
+export const CommentIcon = ({ width = '25px', height = '25px' }) => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width={width}
+        height={height}
+        viewBox="0 0 48 48"
+        id="Bubble_Ellipsis_Right_Fill-7ce6d3b3"
+    >
+        <path
+            fillRule="evenodd"
+            d="M2 21.5c0-10.22 9.88-18 22-18s22 7.78 22 18c0 5.63-3.19 10.74-7.32 14.8a43.6 43.6 0 0 1-14.14 9.1A1.5 1.5 0 0 1 22.5 44v-5.04C11.13 38.4 2 31.34 2 21.5M14 25a3 3 0 1 0 0-6 3 3 0 0 0 0 6m10 0a3 3 0 1 0 0-6 3 3 0 0 0 0 6m13-3a3 3 0 1 1-6 0 3 3 0 0 1 6 0"
+            clipRule="evenodd"
+        ></path>
+    </svg>
+);
+export const SaveIcon = ({ width = '25px', height = '25px' }) => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width={width}
+        height={height}
+        fill="currentColor"
+        viewBox="0 0 24 24"
+    >
+        <path
+            fill="currentColor"
+            d="M4 4.5a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v15.13a1 1 0 0 1-1.555.831l-6.167-4.12a.5.5 0 0 0-.556 0l-6.167 4.12A1 1 0 0 1 4 19.63z"
+        ></path>
+        <path
+            fill="currentColor"
+            fillOpacity=".03"
+            d="M4.032 4.144Q4 4.317 4 4.5v15.13a1 1 0 0 0 1.555.831l6.167-4.12a.5.5 0 0 1 .41-.066l-.427-.198a1.49 1.49 0 0 0-1.377.063c-.581.339-1.45.85-2.25 1.339-.59.359-1.427.695-2.187.962-.929.325-1.86-.387-1.86-1.37zm8.251 12.202 6.162 4.115A1 1 0 0 0 20 19.63V4.5a2 2 0 0 0-1.123-1.798c.21.254.334.58.33.936a117 117 0 0 1-.896 13.408c-.124.99-1.17 1.553-2.076 1.133z"
+        ></path>
+    </svg>
+);
+export const ShareIcon = ({ width = '25px', height = '25px' }) => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width={width}
+        height={height}
+        fill="currentColor"
+        viewBox="0 0 24 24"
+    >
+        <path
+            fill="currentColor"
+            fillRule="evenodd"
+            d="M10.938 3.175a.674.674 0 0 1 1.138-.488l6.526 6.215c.574.547.554 1.47-.043 1.991l-6.505 5.676a.674.674 0 0 1-1.116-.508V13.49s-6.985-1.258-9.225 2.854c-.209.384-1.023.518-.857-1.395.692-3.52 2.106-9.017 10.082-9.017z"
+            clipRule="evenodd"
+        ></path>
+        <path
+            fill="#161823"
+            fillRule="evenodd"
+            d="m15.754 6.212 1.295 2.59a1.12 1.12 0 0 1-.268 1.349l-5.799 5.042s-.28 1.403.562 1.403 7.578-6.174 7.578-6.174.28-.842-.561-1.684c-.843-.842-2.807-2.526-2.807-2.526"
+            clipRule="evenodd"
+            opacity=".03"
+        ></path>
+        <path
+            fill="url(#pc-share-078b3fae_a)"
+            fillRule="evenodd"
+            d="M10.937 6.23v7.297s-6.683-.942-8.777 2.246C.146 18.839.331 12.309 3.363 9.057s7.574-2.827 7.574-2.827"
+            clipRule="evenodd"
+            opacity=".09"
+        ></path>
+        <defs>
+            <radialGradient
+                id="pc-share-078b3fae_a"
+                cx="0"
+                cy="0"
+                r="1"
+                gradientTransform="rotate(-113.046 11.628 5.43)scale(8.93256 8.78076)"
+                gradientUnits="userSpaceOnUse"
+            >
+                <stop></stop>
+                <stop offset=".995" stop-opacity=".01"></stop>
+                <stop offset="1" stop-opacity=".01"></stop>
+            </radialGradient>
+        </defs>
+    </svg>
+);
+export const FeedFollow = ({ width = '14px', height = '14px' }) => (
+    <svg
+        fill="currentColor"
+        viewBox="0 0 48 48"
+        xmlns="http://www.w3.org/2000/svg"
+        width={width}
+        height={height}
+    >
+        <path d="M26 7a1 1 0 0 0-1-1h-2a1 1 0 0 0-1 1v15H7a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h15v15a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1V26h15a1 1 0 0 0 1-1v-2a1 1 0 0 0-1-1H26V7Z"></path>
+    </svg>
+);
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,7 +2,15 @@ import { useRef } from 'react';
 import classNames from 'classnames/bind';
 import styles from './HomePage.module.scss';
 import videos from '~/assets/videos';
-import { CommentIcon, LikeIcon } from '~/components/Icon';
+import {
+    CommentIcon,
+    FeedFollow,
+    LikeIcon,
+    SaveIcon,
+    ShareIcon,
+} from '~/components/Icon';
+import userAvatar from '~/assets/images/user_avatar.jpg';
+import Image from '~/components/Image';
 const cx = classNames.bind(styles);
 function Home() {
     const videoRef = useRef(false);
@@ -27,13 +35,32 @@ function Home() {
                 </div>
             </div>
             <div className={cx('tools')}>
-                <div>Avatar</div>
+                <div className={cx('avatar')}>
+                    <Image
+                        className={cx('user-avatar')}
+                        src={userAvatar}
+                        alt="avatar"
+                    />
+                    <div className={cx('feed-follow')}>
+                        <FeedFollow />
+                    </div>
+                </div>
                 <button>
                     <LikeIcon />
+                    <span>100</span>
                 </button>
-                <button></button>
-                <button></button>
-                <button></button>
+                <button>
+                    <CommentIcon />
+                    <span>5</span>
+                </button>
+                <button>
+                    <SaveIcon />
+                    <span>3</span>
+                </button>
+                <button>
+                    <ShareIcon />
+                    <span>2</span>
+                </button>
             </div>
         </div>
     );
